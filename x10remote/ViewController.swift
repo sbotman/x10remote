0c217755068f3e7bb79ed6b9f32c5a9a63586144
Sander Botman <sander.botman@gmail.com>
2015-12-06 17:49:31 +0100

    Initial Commit

---
+++ x10remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  x10remote
+//
+//  Created by Sander Botman on 12/6/15.
+//  Copyright © 2015 Botman Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
